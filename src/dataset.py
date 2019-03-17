16d47e838d345582c3cefd12143912afe9bf0fda
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-03-17 18:27:41 +0530

    dataset change

--- src/dataset.py
+++ src/dataset.py
@@ -210,7 +210,8 @@ if __name__ == '__main__':
     dataloader = iter(dataloader)
     for i in range(0, len(dataloader) - 1):
         embA, predA  = next(dataloader)
-        countZ, zero = 0, np.zeros((_EMB_DIM))
+        embA.unsqueeze(1)
+        #countZ, zero = 0, np.zeros((_EMB_DIM))
         print(embA.shape, predA.shape) #, embB.shape, predB.shape)
     '''
     for i in range(0, 80):
